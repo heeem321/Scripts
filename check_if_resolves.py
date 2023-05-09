d12f6ceae7bcb14794abb70a97ed2b916428bd51
Ebrahim. Alhussaini <heeem321@hotmail.com>
2023-05-09 14:16:07 +0800

    Create check_if_resolves.py
    initial commit

---
+++ check_if_resolves.py
@@ -0,0 +1,15 @@
+import socket
+import sys
+
+# Get the list of domains from the command line arguments
+domains = sys.argv[1:]
+
+# Loop through each domain and check if it resolves
+for domain in domains:
+    try:
+        # Use the gethostbyname function to get the IP address for the domain
+        ip = socket.gethostbyname(domain)
+        print(f"{domain} resolves to {ip}")
+    except socket.gaierror:
+        # If the domain doesn't resolve, print an error message
+        print(f"{domain} does not resolve")
